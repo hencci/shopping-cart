fa92d82e74074efc2489936a93c4afa57b0d93f9
hencci <hencci20@gmail.com>
2025-10-25 07:07:08 +0100

    Reove unused files and dependencies

--- vite.config.js
+++ vite.config.js
@@ -4,9 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    globals: true, // allows global test(), expect(), etc.
-    environment: "happy-dom", // simulates the browser environment
-    setupFiles: "./src/setupTests.js", // setup file for jest-dom
-  },
 });
